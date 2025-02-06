225a5f99334fa0918f66553707f970a4fa756c20
Matheus Rafael <matheusrafael77@hotmail.com>
2025-02-05 23:17:30 -0300

    feat: Repositorios criados

---
+++ src/main/java/com/api/library/repositories/EmprestimoRepository.java
@@ -0,0 +1,9 @@
+package com.api.library.repositories;
+
+import com.api.library.entities.Emprestimo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmprestimoRepository extends JpaRepository<Emprestimo, Integer> {
+}
