90b6d0baa890ba4b1aadd6da7c42289fa225c628
Matheus Rafael <matheusrafael77@hotmail.com>
2025-02-06 21:50:54 -0300

    feat: finalizado cruds de livros

--- src/main/java/com/api/library/controllers/LivroController.java
+++ src/main/java/com/api/library/controllers/LivroController.java
@@ -1,10 +1,51 @@
 package com.api.library.controllers;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.api.library.dtos.LivroDTO;
+import com.api.library.services.LivroService;
+import jakarta.validation.Valid;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/livros")
+@Data
 public class LivroController {
+    private final LivroService service;
+
+    @Autowired
+    public LivroController(LivroService service) {
+        this.service = service;
+    }
+
+    @GetMapping("find-by-id/{id}")
+    public ResponseEntity<LivroDTO> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok().body(service.findLivroById(id));
+    }
+
+    @GetMapping("/find-all")
+    public ResponseEntity<List<LivroDTO>> findAll() {
+        return ResponseEntity.ok().body(service.findLivroList());
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<LivroDTO> save(@RequestBody @Valid LivroDTO livroDTO) {
+        return ResponseEntity.ok().body(service.createLivro(livroDTO));
+    }
+
+    @PatchMapping("/patch")
+    public ResponseEntity<LivroDTO> patch(@RequestBody @Valid LivroDTO livroDTO) {
+        return ResponseEntity.ok().body(service.patchLivro(livroDTO));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        service.deleteLivro(id);
+        return ResponseEntity.ok().build();
+    }
+
 }
--- src/main/java/com/api/library/services/LivroService.java
+++ src/main/java/com/api/library/services/LivroService.java
@@ -1,7 +1,52 @@
 package com.api.library.services;
 
+import com.api.library.dtos.LivroDTO;
+import com.api.library.entities.Livro;
+import com.api.library.exceptions.NotFoundException;
+import com.api.library.repositories.LivroRepository;
+import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class LivroService {
+    private final LivroRepository repository;
+    private final ModelMapper mapper;
+
+    @Autowired
+    public LivroService(LivroRepository repository, ModelMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public List<LivroDTO> findLivroList() {
+        return repository.findAll().stream().map((livro -> mapper.map(livro, LivroDTO.class))).toList();
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public LivroDTO findLivroById(Integer id) {
+        Livro livro = repository.findById(id).orElseThrow(() -> new NotFoundException("Livro não encontrado"));
+        return mapper.map(livro, LivroDTO.class);
+    }
+
+    public LivroDTO createLivro(LivroDTO livroDTO) {
+        Livro livroEntity = repository.save(mapper.map(livroDTO, Livro.class));
+        return mapper.map(livroEntity, LivroDTO.class);
+    }
+
+    public LivroDTO patchLivro(@Valid LivroDTO livroDTO) {
+        Livro livro = repository.findById(livroDTO.getId()).orElseThrow(() -> new NotFoundException("Livro não encontrado"));
+        BeanUtils.copyProperties(livroDTO, livro);
+        return mapper.map(repository.save(livro), LivroDTO.class);
+    }
+
+    public void deleteLivro(Integer id) {
+        this.repository.deleteById(id);
+    }
 }
